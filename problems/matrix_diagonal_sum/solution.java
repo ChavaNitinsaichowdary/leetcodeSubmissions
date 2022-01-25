a470630568a14f0d39dbeb354932562c95586108
Chava Nitin sai chowdary <105057405+ChavaNitinsaichowdary@users.noreply.github.com>
2022-01-25 12:38:07 +0000

    Sync LeetCode submission - Matrix Diagonal Sum (java)

--- problems/matrix_diagonal_sum/solution.java
+++ problems/matrix_diagonal_sum/solution.java
@@ -1,15 +1,14 @@
 class Solution {
     public int diagonalSum(int[][] mat) {
-        int n= mat.length;
-        int m=mat[0].length;
-        int s=0;
-        for(int i=0;i<n;i++){
-            s+=mat[i][i]+mat[i][n-i-1];
-        }
-        if(n%2!=0){
-            s=s-mat[n/2][n/2];
-        }
-        return s;
-        
+       int sum = 0;
+       int i=0;
+       while(i<mat.length){
+           sum+=mat[i][i]+mat[i][mat.length-i-1];
+           i++;
+       }
+       if(mat.length%2==0){
+           return sum;
+       }
+       return (sum-(mat[mat.length/2][mat.length/2]));
     }
 }
