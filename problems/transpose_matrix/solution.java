44b2ee9d8530ea4d0e1e0d236ac8803c06d71584
Chava Nitin sai chowdary <105057405+ChavaNitinsaichowdary@users.noreply.github.com>
2022-01-25 11:51:42 +0000

    Sync LeetCode submission - Transpose Matrix (java)

--- problems/transpose_matrix/solution.java
+++ problems/transpose_matrix/solution.java
@@ -1,15 +1,11 @@
 class Solution {
     public int[][] transpose(int[][] matrix) {
-        
-        int m=matrix.length;
-        int n=matrix[0].length;
-        int[][] ans = new int[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        int[][] ans = new int[matrix[0].length][matrix.length];
+        for(int i = 0;i<matrix[0].length;i++){
+            for(int j = 0;j<matrix.length;j++){
                 ans[i][j]=matrix[j][i];
             }
         }
         return ans;
-        
     }
 }
