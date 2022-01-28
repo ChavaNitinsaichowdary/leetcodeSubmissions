681790c24ef01e5d0a200256de1b02fc779b8d3f
Chava Nitin sai chowdary <105057405+ChavaNitinsaichowdary@users.noreply.github.com>
2022-01-28 13:18:21 +0000

    Sync LeetCode submission - Special Array With X Elements Greater Than or Equal X (java)

--- problems/special_array_with_x_elements_greater_than_or_equal_x/solution.java
+++ problems/special_array_with_x_elements_greater_than_or_equal_x/solution.java
@@ -1,20 +1,22 @@
 class Solution {
     public int specialArray(int[] nums) {
-       int x = 1;
-       int count = 0;
-    for( x = 1;x<=nums.length;x++){
-       for(int i =0;i<nums.length;i++){
-           if(nums[i]>=x){
-               count++;
-           }
-         }
-         if(count==x){
-             return x;
-             
-         }
-        count = 0;
+        Arrays.sort(nums);
+        for(int i = 0 ; i < 101 ; i++){
+            if(i == nums.length-leftMostIndex(nums,i)) return i ;
+        }
+        return -1;
     }
-    return -1;
-       
+	// function to get left most value
+    public  int leftMostIndex(int[] nums ,int target){
+       int left = 0 , right = nums.length-1;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if(nums[mid]>=target){
+                right = mid-1;
+            }else if(nums[mid]<target){
+                left = mid+1;
+            }
+        }
+        return left ;  //we can return either left or mid as both pointers point same index
     }
 }
