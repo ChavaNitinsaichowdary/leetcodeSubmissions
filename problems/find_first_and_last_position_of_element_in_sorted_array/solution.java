cd81241738762c4af01c932a5b6b8f025332ffbd
Chava Nitin sai chowdary <105057405+ChavaNitinsaichowdary@users.noreply.github.com>
2022-01-28 10:24:48 +0000

    Sync LeetCode submission - Find First and Last Position of Element in Sorted Array (java)

--- problems/find_first_and_last_position_of_element_in_sorted_array/solution.java
+++ problems/find_first_and_last_position_of_element_in_sorted_array/solution.java
@@ -1,32 +1,36 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] arr = new int[2];
-     arr[0]= search(nums,target,true);
-        if(arr[0]!=-1){
-          arr[1] = search(nums,target,false);
+       int[] ans = new int[2];
+       ans[0]=searchIndex(nums,target,true);
+       if(ans[0]!=-1){
+           ans[1]=searchIndex(nums,target,false);
+       }
+        else{
+            ans[1]=-1;
         }
-        else return new int[]{-1,-1};
-        return arr;
+        return ans;
     }
-    public int search(int[] nums,int target,boolean firstIndex){
-        int s =0;
-        int e =nums.length-1;
-        int ans = -1;
+    public int searchIndex(int[] nums,int target,boolean firstIndex){
+        int s = 0;
+        int e = nums.length-1;
+        int ans=-1;
         while(e>=s){
             int m = s+(e-s)/2;
             if(target>nums[m]){
                 s=m+1;
             }
-            else if(target<nums[m]){
+            if(target<nums[m]){
                 e=m-1;
             }
-            else{
-                ans = m;
+            if(target==nums[m]){
+                ans=m;
                 if(firstIndex){
                     e=m-1;
                 }
-                else s=m+1;
-            }   
+                else{
+                    s=m+1;
+                }
+            }
         }
         return ans;
     }
