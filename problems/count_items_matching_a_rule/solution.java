879f87500464689fe03a2232ad862174fca69251
Chava Nitin sai chowdary <105057405+ChavaNitinsaichowdary@users.noreply.github.com>
2022-01-30 10:50:40 +0000

    Sync LeetCode submission - Count Items Matching a Rule (java)

--- problems/count_items_matching_a_rule/solution.java
+++ problems/count_items_matching_a_rule/solution.java
@@ -1,27 +1,13 @@
 class Solution {
     public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
-        int ans=0;
-        if(ruleKey.equals("type")){
-           for(int i =0;i<items.size();i++){
-             if(items.get(i).get(0).equals(ruleValue)){
-                 ans++;
-             }
-           }   
+        int res = 0;
+        
+        for(int i = 0 ;i<items.size();i++){
+            if(ruleKey.equals("type") && items.get(i).get(0).equals(ruleValue)) res++;
+            if(ruleKey.equals("color") && items.get(i).get(1).equals(ruleValue)) res++;
+            if(ruleKey.equals("name") && items.get(i).get(2).equals(ruleValue)) res++;
         }
-        else  if(ruleKey.equals("color")){
-           for(int i =0;i<items.size();i++){
-             if(items.get(i).get(1).equals(ruleValue)){
-                 ans++;
-             }
-           }   
-        }
-        else if(ruleKey.equals("name")){
-           for(int i =0;i<items.size();i++){
-             if(items.get(i).get(2).equals(ruleValue)){
-                 ans++;
-             }
-           }   
-        }
-        return ans;
+        
+        return res;
     }
 }
