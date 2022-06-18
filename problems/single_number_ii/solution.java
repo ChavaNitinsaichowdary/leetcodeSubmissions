fbea86aa677a272519922760b47407ba08fede56
Chava Nitin sai chowdary <105057405+ChavaNitinsaichowdary@users.noreply.github.com>
2022-06-18 05:40:15 +0000

    Sync LeetCode submission - Single Number II (java)

--- problems/single_number_ii/solution.java
+++ problems/single_number_ii/solution.java
@@ -1,18 +1,10 @@
 class Solution {
-    public int singleNumber(int[] nums) {
-        int ans = 0;
-        for(int i = 0;i<32;i++){
-            int sum = 0;
-            for(int j=0;j<nums.length;j++){
-                if((nums[j]>>i & 1)==1){
-                    sum++;
-                    sum=sum%3;
-                }
-            }
-            if(sum!=0){
-                ans=ans|sum<<i;
-            }
+    public int singleNumber(int[] A) {
+        int ones = 0, twos = 0;
+        for(int i = 0; i < A.length; i++){
+           ones = (ones ^ A[i]) & ~twos;
+           twos = (twos ^ A[i]) & ~ones;
         }
-        return ans;
+        return ones;
     }
 }
