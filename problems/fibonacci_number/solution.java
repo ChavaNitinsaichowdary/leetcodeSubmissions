2d8fdec874318109d0811dd90f3f7abb72fdd866
Chava Nitin sai chowdary <105057405+ChavaNitinsaichowdary@users.noreply.github.com>
2022-07-12 08:19:10 +0000

    Sync LeetCode submission - Fibonacci Number (java)

--- problems/fibonacci_number/solution.java
+++ problems/fibonacci_number/solution.java
@@ -1,8 +1,32 @@
 class Solution {
+    static int[] dp = new int[31];
     public int fib(int n) {
-      if(n<2){
+      
+      if(n<=1){
           return n;
       }
-      return fib(n-1)+fib(n-2);
+      if(dp[n]!=0){
+          return dp[n];
+      }
+      int left = 0;
+      if(dp[n-1]!=0){
+          left = dp[n-1];
+      }
+      else{
+          left = fib(n-1);
+          dp[n-1] = left;
+      }
+      int right = 0;
+      if(dp[n-2]!=0){
+          right = dp[n-2];
+      }
+      else{
+          right = fib(n-2);
+          dp[n-2] = right;
+      }
+      dp[n] = left+right;
+      return dp[n];
+        
+        
     }
 }
