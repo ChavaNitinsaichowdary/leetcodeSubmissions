bff368e8db5de427a4600ce19b30cc9fc6aecea4
Chava Nitin sai chowdary <105057405+ChavaNitinsaichowdary@users.noreply.github.com>
2022-12-23 16:18:41 +0000

    Sync LeetCode submission - Sqrt(x) (java)

--- problems/sqrt(x)/solution.java
+++ problems/sqrt(x)/solution.java
@@ -1,27 +1,8 @@
 class Solution {
     public int mySqrt(int x) {
-            int left = 0, right = x;
-        int res = -1;
-        while(left <= right){
-            int mid = left + (right - left) / 2;
-            
-            long square = (long)mid * mid;
-
-            if(square == x){
-                return mid;
-            }
-            
-            else if(square < x){
-                res = mid;
-                left = mid + 1;
-            }
-            
-            else{
-                right = mid - 1;
-            }
-        }
-        return res;
-
-        
+            long r = x;
+            while (r*r > x)
+                  r = (r + x/r) / 2;
+            return (int) r;
     }
 }
