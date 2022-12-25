2636bb14696037bcce31e7ef2d651bc2f4c3a63b
Chava Nitin sai chowdary <105057405+ChavaNitinsaichowdary@users.noreply.github.com>
2022-12-25 06:17:37 +0000

    Sync LeetCode submission - Guess Number Higher or Lower (java)

--- problems/guess_number_higher_or_lower/solution.java
+++ problems/guess_number_higher_or_lower/solution.java
@@ -1,30 +1,23 @@
 /** 
  * Forward declaration of guess API.
  * @param  num   your guess
- * @return 	     -1 if num is lower than the guess number
- *			      1 if num is higher than the guess number
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
  *               otherwise return 0
  * int guess(int num);
  */
 
 public class Solution extends GuessGame {
     public int guessNumber(int n) {
-       int s = 1;
-       int e = n;
-       while(e>=s){
-           int mid = s+(e-s)/2;
-           if(guess(mid)==0){
-               return mid;
-           }
-           else if(guess(mid)==1){
-               s = mid+1;
-           }
-           else{
-               e = mid-1;
-           }
-       }
-       return -1;
-        
+        return helper(1,n);
     }
-    
+    public int helper(int start,int end){
+        if(start == end) return start;
+        int mid = Math.toIntExact(((long)start+(long)end)/2);
+        int r = 0;
+        if(guess(mid) == 0) r = mid;
+        else if(guess(mid) == 1) r = helper(mid+1, end);
+        else if(guess(mid) == -1) r = helper(start, mid-1);
+        return r;
+    }
 }
