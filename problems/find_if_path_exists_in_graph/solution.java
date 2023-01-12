a7fbbca2dd6159948b6870358eeab457579a9d41
Chava Nitin sai chowdary <105057405+ChavaNitinsaichowdary@users.noreply.github.com>
2023-01-12 04:26:48 +0000

    Sync LeetCode submission - Find if Path Exists in Graph (java)

--- problems/find_if_path_exists_in_graph/solution.java
+++ problems/find_if_path_exists_in_graph/solution.java
@@ -1,21 +1,30 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i = 1;i<=n;i++) adj.add(new ArrayList<Integer>());
-        for(int[] edge:edges){
-            adj.get(edge[0]).add(edge[1]);
-            adj.get(edge[1]).add(edge[0]);
+        for(int i = 0;i<n;i++){
+            adj.add(new ArrayList<Integer>());
         }
-        boolean[] visited = new boolean[n];
-        return dfs(source,destination,adj,visited);
-    }
-    public boolean dfs(int source,int destination,ArrayList<ArrayList<Integer>> adj,boolean[] visited){
         if(source==destination) return true;
-        if(visited[source]) return false;
-        visited[source] = true;
-        for(Integer it: adj.get(source)){
-            if(!visited[it]){
-                if(dfs(it,destination,adj,visited)) return true;
+        for(int[] edge: edges){
+              adj.get(edge[0]).add(edge[1]);
+              adj.get(edge[1]).add(edge[0]);
+        }
+        
+        
+        boolean visited[] = new boolean[n];
+        Stack<Integer> st = new Stack<>();
+        st.push(source);
+        visited[source]=true;
+        while(!st.isEmpty()){
+            int curr_node = st.pop();
+            for(Integer nextNode:adj.get(curr_node)){
+                if(!visited[nextNode]){
+                    if(nextNode==destination){
+                        return true;
+                    }
+                    st.push(nextNode);
+                    visited[nextNode] = true;
+                }
             }
         }
         return false;
