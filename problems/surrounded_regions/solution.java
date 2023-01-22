932fbe160b21bcea372c7fef5ed54e032df6787f
Chava Nitin sai chowdary <105057405+ChavaNitinsaichowdary@users.noreply.github.com>
2023-01-22 06:42:05 +0000

    Sync LeetCode submission - Surrounded Regions (java)

--- problems/surrounded_regions/solution.java
+++ problems/surrounded_regions/solution.java
@@ -1,46 +1,56 @@
 class Solution {
-    public void solve(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        Queue<Integer> q = new LinkedList<>();
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                if(i==0||j==0||i==m-1||j==n-1){
-                   if(grid[i][j]=='O'){
-                        q.add(i*n+j);
-                        grid[i][j]='s';
-                   } 
-                }
+    public void solve(char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        for(int i = 0;i<board.length;i++){
+            if(board[i][0]=='O'){
+                board[i][0]='t';
+                q.add(new int[]{i,0});
+            }
+            if(board[i][board[0].length-1]=='O'){
+                board[i][board[0].length-1]='t';
+                q.add(new int[]{i,n-1});
             }
         }
-        int[][] dir = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+        for(int i = 0;i<board[0].length;i++){
+            if(board[0][i]=='O'){
+                board[0][i]='t';
+                q.add(new int[]{0,i});
+            }
+            if(board[board.length-1][i]=='O'){
+                board[board.length-1][i]='t';
+                q.add(new int[]{m-1,i});
+            }
+        }
+
+        int[][] dir = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
         while(!q.isEmpty()){
-            int sz = q.size();
-            while(sz-->0){
-                int currCell = q.poll();
-                int r = currCell/n;
-                int c = currCell%n;
-                for(int k = 0;k<dir.length;k++){
-                    int nr = r+dir[k][0];
-                    int nc = c+dir[k][1];
-                    if(nr>=0&&nc>=0&&nr<m&&nc<n&&grid[nr][nc]=='O'){
-                        q.add(nr*n+nc);
-                        grid[nr][nc]='s';
+            int[] curr = new int[2];
+            curr = q.poll();
+            int currR = curr[0];
+            int currC = curr[1];
+            for(int i = 0;i<dir.length;i++){
+                int nextR = currR+dir[i][0];
+                int nextC = currC+dir[i][1];
+                if(nextR<board.length&&nextR>=0&&nextC<board[0].length&&nextC>=0){
+                    if(board[nextR][nextC]=='O'){
+                      q.add(new int[]{nextR,nextC});
+                      board[nextR][nextC]='t';
                     }
                 }
+                
             }
         }
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                if(grid[i][j]=='O'){
-                    grid[i][j]='X';
+        for(int i = 0;i<board.length;i++){
+            for(int j = 0;j<board[0].length;j++){
+                if(board[i][j]=='t'){
+                    board[i][j]='O';
                 }
-               else if(grid[i][j]=='s'){
-                    grid[i][j]='O';
+                else{
+                    board[i][j]='X';
                 }
             }
         }
-        return;
-        
     }
 }
