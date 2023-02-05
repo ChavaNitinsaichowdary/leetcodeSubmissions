0ff46435b22d842044367f8a4802c6416973d25a
Chava Nitin sai chowdary <105057405+ChavaNitinsaichowdary@users.noreply.github.com>
2023-02-05 12:41:44 +0000

    Sync LeetCode submission - Find All Anagrams in a String (java)

--- problems/find_all_anagrams_in_a_string/solution.java
+++ problems/find_all_anagrams_in_a_string/solution.java
@@ -1,40 +1,34 @@
-class Solution {
-    public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> list = new ArrayList<>();
-        if(p.length()>s.length()){
-            return list; 
-        }
-        int begin = 0;
-        int end = 0;
-        Map<Character,Integer> m = new HashMap<>();
-        for(char ch : p.toCharArray()){
-            m.put(ch,m.getOrDefault(ch,0)+1);
-        }
-        int counter = m.size();
-        while(end<s.length()){
-            char ch = s.charAt(end);
-            if(m.containsKey(ch)){
-                m.put(ch,m.get(ch)-1);
-                if(m.get(ch)==0){
-                    counter--;
-                }
+		class Solution {
+		public List<Integer> findAnagrams(String s, String p) {
+			List<Integer> ans = new ArrayList<>();
+            int n = s.length();
+            int m = p.length();
+			if(m>n){
+                return ans;
             }
-            end++;
-            while(counter==0){
-                char temp = s.charAt(begin);
-                if(m.containsKey(temp)){
-                    m.put(temp,m.get(temp)+1);
-                    if(m.get(temp)>0){
-                        counter++;
-                    }
+			int []arrs = new int[26];
+			int []arrp = new int[26];
+			for(int i=0;i<m;i++){
+				arrs[s.charAt(i)-'a']++;
+				arrp[p.charAt(i)-'a']++;
+			}
+			for(int i=0;i<=n-m;i++){
+			    if(iszero(arrs,arrp)){
+                    ans.add(i);
                 }
-                if((end-begin)==p.length()){
-                    list.add(begin);
+				arrs[s.charAt(i)-'a']--;
+				if(i+m < n){
+                    arrs[s.charAt(i+m)-'a']++;
                 }
-                
-                begin++;
-            }
+			}
+			return ans;
+		}
+        public boolean iszero(int [] arrs, int []arrp){
+            for(int j=0;j<26;j++){
+				if(arrs[j]!=arrp[j]){
+					return false;
+				    }
+				}
+            return true;
         }
-        return list;
-    }
-}
+	}
