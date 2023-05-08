163488b0f24cf2cf2b1098e914881d0b91e9810a
Chava Nitin sai chowdary <105057405+ChavaNitinsaichowdary@users.noreply.github.com>
2023-05-08 08:50:17 +0000

    Sync LeetCode submission - Two Sum (java)

--- problems/two_sum/solution.java
+++ problems/two_sum/solution.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> h = new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    return new int[]{i,j};
-                }
+            if(h.containsKey(target-nums[i])){
+                return new int[]{h.get(target-nums[i]),i};
             }
+            h.put(nums[i],i);
         }
         return new int[]{-1,-1};
     }
