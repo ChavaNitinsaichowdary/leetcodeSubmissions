454c935010a2a1ea5676df55fc9ad3dec87fa15c
Chava Nitin sai chowdary <105057405+ChavaNitinsaichowdary@users.noreply.github.com>
2023-08-08 13:19:34 +0000

    Sync LeetCode submission - Binary Tree Right Side View (java)

---
+++ problems/binary_tree_right_side_view/solution.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        if(root==null) return new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        ArrayList<Integer> ans = new ArrayList<>();
+       
+        while(!q.isEmpty()){
+            int sz = q.size();
+            while(sz-->0){
+                TreeNode curr = q.poll();
+                if(sz==0) ans.add(curr.val);
+                if(curr.left!=null){
+                    q.offer(curr.left);
+                }
+                if(curr.right!=null){
+                    q.offer(curr.right);
+                }
+            }
+        }
+        return ans;
+    }
+}
